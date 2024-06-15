1ae0040a6bb16eda245f050e3533cdceddcec5b8
pha2168 <81567759+pha2168@users.noreply.github.com>
2024-06-16 01:48:58 +0900

    Door.cs

--- Script/Door.cs
+++ Script/Door.cs
@@ -9,10 +9,8 @@ public class Door : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // 충돌한 오브젝트가 플레이어인지 확인
         if (collision.CompareTag("Player"))
         {
-            // 씬을 로드
             SceneManager.LoadScene(sceneToLoad);
         }
     }
