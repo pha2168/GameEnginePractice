a9ae3bc8600f8c636f2d3ed064489f1ce4dc3a1e
pha2168 <81567759+pha2168@users.noreply.github.com>
2024-06-16 01:57:45 +0900

    playButton.cs

--- Script/playButton.cs
+++ Script/playButton.cs
@@ -5,25 +5,22 @@ using UnityEngine.UI;
 
 public class playButton : MonoBehaviour
 {
-    public GameObject videoCanvas;  // videoCanvas 오브젝트를 연결
-    public Camera mainCamera;       // 메인 카메라 오브젝트를 연결
-    public Button myButton;         // 버튼 오브젝트를 연결
+    public GameObject videoCanvas;
+    public Camera mainCamera;
+    public Button myButton;
 
     void Start()
     {
-        // 버튼 클릭 이벤트에 OnButtonClick 메서드 연결
         myButton.onClick.AddListener(OnButtonClick);
     }
 
     void OnButtonClick()
     {
-        // videoCanvas 오브젝트 활성화
         if (videoCanvas != null)
         {
             videoCanvas.SetActive(true);
         }
 
-        // 메인 카메라 오디오 소스 비활성화
         if (mainCamera != null)
         {
             AudioSource audioSource = mainCamera.GetComponent<AudioSource>();
