3bfe4e92ba1370ac506815f5443e6f5d384730ed
pha2168 <81567759+pha2168@users.noreply.github.com>
2024-06-16 01:55:47 +0900

    Player.cs

--- Script/Player.cs
+++ Script/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
     public GameObject arrowPrefab;
     public Transform arrowSpawnPoint;
     public LayerMask groundLayer;
-    public Transform groundCheck; // 플레이어 발밑에 위치한 작은 빈 게임 오브젝트
+    public Transform groundCheck;
 
     private Animator animator;
     private Rigidbody2D rb;
@@ -35,10 +35,8 @@ public class Player : MonoBehaviour
 
         if (move != 0)
         {
-            // 이동
             transform.Translate(Vector3.right * move * moveSpeed * Time.deltaTime);
 
-            // 플레이어 회전
             if (move > 0 && !isFacingRight)
             {
                 Flip();
@@ -48,12 +46,10 @@ public class Player : MonoBehaviour
                 Flip();
             }
 
-            // 걷기 애니메이션 재생
             animator.SetBool("Walk", true);
         }
         else
         {
-            // 걷기 애니메이션 중지
             animator.SetBool("Walk", false);
         }
     }
@@ -62,10 +58,8 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.X))
         {
-            // 공격 애니메이션 재생
             animator.SetTrigger("Attack");
 
-            // 일정 시간 후에 화살 소환
             Invoke("SpawnArrow", 0.5f);
         }
     }
@@ -76,28 +70,22 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            // 점프 애니메이션 재생
             animator.SetTrigger("Jump");
 
-            // 점프
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
     }
 
     void SpawnArrow()
     {
-        // 화살 소환
         GameObject arrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, Quaternion.identity);
 
-        // 화살의 방향을 플레이어의 방향에 맞게 설정
         if (!isFacingRight)
         {
-            // 왼쪽을 볼 때
             arrow.transform.rotation = Quaternion.Euler(0, 0, 90);
         }
         else
         {
-            // 오른쪽을 볼 때
             arrow.transform.rotation = Quaternion.Euler(0, 0, -90);
         }
     }
