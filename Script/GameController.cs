38a826f9ef9bc1cb6dfdcccfabb4440aa30fc125
pha2168 <81567759+pha2168@users.noreply.github.com>
2024-06-16 01:52:49 +0900

    GameController.cs

--- Script/GameController.cs
+++ Script/GameController.cs
@@ -8,14 +8,12 @@ public class PersistantObject : MonoBehaviour
 
     void Awake()
     {
-        // 이미 인스턴스가 있으면 파괴
         if (instance != null && instance != this)
         {
             Destroy(gameObject);
             return;
         }
 
-        // 인스턴스가 없으면 유지
         instance = this;
         DontDestroyOnLoad(gameObject);
     }
